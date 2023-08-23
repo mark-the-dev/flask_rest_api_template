102203a2236b17dc1f627e251c0f9d7ec97caff1
Mark Maluenda <markmaluenda@Marks-MacBook-Pro.local>
2023-08-23 13:55:23 -0400

    Example test for routes

--- app/test/routes/__init__.py
+++ app/test/routes/__init__.py
@@ -3,4 +3,4 @@ from main import create_app
 
 class TestAPIResource(unittest.TestCase):
     def setUp(self):
-        self.app = create_app("test")
+        self.app = create_app("test").test_client()
